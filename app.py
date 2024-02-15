0f7cdaf6173e7fe7d7cbff4a210e4fb84c95c74d
sang-il <lsi9393@naver.com>
2024-02-15 12:14:26 +0900

    issue #5 멤버별 상세 페이지 이동 구현 * 멤버 클릭시 members 함수로 리다이렉션 * 멤버 데이터 profile로 전달

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, url_for, Response
 import os
+import flask
+from flask import Flask, render_template, request, url_for, Response
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 
@@ -49,25 +49,95 @@ def createbooks():
 
     return render_template('index.html', books=books)
 
-context = {
-    'img_url': 'static/image/rtan00.jpg',
-    'blog_url': 'https://silee-sw.tistory.com/',
-    'name': '이상일',
-    'introduce': '안녕하세요~\n르탄이에요',
-    'programming_languages': ['java', 'python', 'html'],
-    'strength': '멍떄리기',
-    'mbti': 'ISFP',
-    'tmi': '자전거 여행 좋아합니다~ 같이 하실분 DM 주세요~'
+
+member_datas = {
+    1: {
+        'img_url': 'image/rtan00.jpg',
+        'blog_url': 'https://silee-sw.tistory.com/',
+        'name': '이준승',
+        'introduce': '안녕하세요~\n르탄이에요',
+        'programming_languages': ['java', 'python', 'html'],
+        'strength': '멍떄리기',
+        'mbti': 'ISFP',
+        'tmi': '자전거 여행 좋아합니다~ 같이 하실분 DM 주세요~'
+    },
+    2: {
+        'img_url': 'static/image/rtan00.jpg',
+        'blog_url': 'https://silee-sw.tistory.com/',
+        'name': '이상일',
+        'introduce': '안녕하세요~\n르탄이에요',
+        'programming_languages': ['java', 'python', 'html'],
+        'strength': '멍떄리기',
+        'mbti': 'ISFP',
+        'tmi': '자전거 여행 좋아합니다~ 같이 하실분 DM 주세요~'
+    },
+    3: {
+        'img_url': 'static/image/rtan00.jpg',
+        'blog_url': 'https://silee-sw.tistory.com/',
+        'name': '이현기',
+        'introduce': '안녕하세요~\n르탄이에요',
+        'programming_languages': ['java', 'python', 'html'],
+        'strength': '멍떄리기',
+        'mbti': 'ISFP',
+        'tmi': '자전거 여행 좋아합니다~ 같이 하실분 DM 주세요~'
+    },
+    4: {
+        'img_url': 'static/image/rtan00.jpg',
+        'blog_url': 'https://silee-sw.tistory.com/',
+        'name': '권진우',
+        'introduce': '안녕하세요~\n르탄이에요',
+        'programming_languages': ['java', 'python', 'html'],
+        'strength': '멍떄리기',
+        'mbti': 'ISFP',
+        'tmi': '자전거 여행 좋아합니다~ 같이 하실분 DM 주세요~'
+    },
+    5: {
+        'img_url': 'static/image/rtan00.jpg',
+        'blog_url': 'https://silee-sw.tistory.com/',
+        'name': '이규민',
+        'introduce': '안녕하세요~\n르탄이에요',
+        'programming_languages': ['java', 'python', 'html'],
+        'strength': '멍떄리기',
+        'mbti': 'ISFP',
+        'tmi': '자전거 여행 좋아합니다~ 같이 하실분 DM 주세요~'
+    },
 }
 
-@app.route('/members/1')
-def members1():
+@app.route('/members/<memberid>')
+def members(memberid):
     # member
+    memberid = int(memberid)
+    print(f'memberid={memberid}')
+    context = member_datas[memberid]
 
     # replace \n -> <br>
     context['introduce'] = context['introduce'].replace('\n', '<br>')
 
     return render_template('profle.html', data=context)
+
+
+@app.route('/members/member1')
+def members1():
+    return flask.redirect(flask.url_for('members', memberid=1))
+
+
+@app.route('/members/member2')
+def members2():
+    return flask.redirect(flask.url_for('members', memberid=2))
+
+
+@app.route('/members/member3')
+def members3():
+    return flask.redirect(flask.url_for('members', memberid=3))
+
+@app.route('/members/member4')
+def members4():
+    return flask.redirect(flask.url_for('members', memberid=4))
+
+
+@app.route('/members/member5')
+def members5():
+    return flask.redirect(flask.url_for('members', memberid=5))
 
 
 if __name__ == '__main__':
---
+++ test.py
@@ -0,0 +1,57 @@
+
+member_datas = {
+    1: {
+        'img_url': 'static/image/rtan00.jpg',
+        'blog_url': 'https://silee-sw.tistory.com/',
+        'name': '이준승',
+        'introduce': '안녕하세요~\n르탄이에요',
+        'programming_languages': ['java', 'python', 'html'],
+        'strength': '멍떄리기',
+        'mbti': 'ISFP',
+        'tmi': '자전거 여행 좋아합니다~ 같이 하실분 DM 주세요~'
+    },
+    2: {
+        'img_url': 'static/image/rtan00.jpg',
+        'blog_url': 'https://silee-sw.tistory.com/',
+        'name': '이상일',
+        'introduce': '안녕하세요~\n르탄이에요',
+        'programming_languages': ['java', 'python', 'html'],
+        'strength': '멍떄리기',
+        'mbti': 'ISFP',
+        'tmi': '자전거 여행 좋아합니다~ 같이 하실분 DM 주세요~'
+    },
+    3: {
+        'img_url': 'static/image/rtan00.jpg',
+        'blog_url': 'https://silee-sw.tistory.com/',
+        'name': '이현기',
+        'introduce': '안녕하세요~\n르탄이에요',
+        'programming_languages': ['java', 'python', 'html'],
+        'strength': '멍떄리기',
+        'mbti': 'ISFP',
+        'tmi': '자전거 여행 좋아합니다~ 같이 하실분 DM 주세요~'
+    },
+    4: {
+        'img_url': 'static/image/rtan00.jpg',
+        'blog_url': 'https://silee-sw.tistory.com/',
+        'name': '권진우',
+        'introduce': '안녕하세요~\n르탄이에요',
+        'programming_languages': ['java', 'python', 'html'],
+        'strength': '멍떄리기',
+        'mbti': 'ISFP',
+        'tmi': '자전거 여행 좋아합니다~ 같이 하실분 DM 주세요~'
+    },
+    5: {
+        'img_url': 'static/image/rtan00.jpg',
+        'blog_url': 'https://silee-sw.tistory.com/',
+        'name': '이규민',
+        'introduce': '안녕하세요~\n르탄이에요',
+        'programming_languages': ['java', 'python', 'html'],
+        'strength': '멍떄리기',
+        'mbti': 'ISFP',
+        'tmi': '자전거 여행 좋아합니다~ 같이 하실분 DM 주세요~'
+    },
+}
+
+
+print(member_datas[1])
+print(member_datas[5])
