2f611e03042ec96b3b5de10dfdb132328055380a
sang-il <lsi9393@naver.com>
2024-02-15 11:39:07 +0900

    issue #5 멤버별 상세 페이지 이동 구현 * 데이터별 상세 페이지 표시

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,url_for, Response
+from flask import Flask, render_template, request, url_for, Response
 import os
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,25 +6,31 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
-db = SQLAlchemy(app) 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'database.db')
+db = SQLAlchemy(app)
+
 
 @app.route('/', methods=['Get'])
 def home():
-    
+
     books = BookMsg.query.order_by('id').all()
     print(books)
     return render_template('index.html', books=books)
 
+
 class BookMsg(db.Model):
-            id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-            name = db.Column(db.String, nullable=False)
-            message = db.Column(db.String, nullable=False)
-            
-            def __repr__(self):
-                return f'{self.name} {self.message} 작성자 by {self.name}'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    message = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'{self.name} {self.message} 작성자 by {self.name}'
+
+
 with app.app_context():
     db.create_all()
+
 
 @app.route('/Books/create/', methods=['Post'])
 def createbooks():
@@ -40,14 +46,29 @@ def createbooks():
     db.session.commit()
 
     books = BookMsg.query.order_by('id').all()
-        
+
     return render_template('index.html', books=books)
+
+context = {
+    'img_url': 'static/image/rtan00.jpg',
+    'blog_url': 'https://silee-sw.tistory.com/',
+    'name': '이상일',
+    'introduce': '안녕하세요~\n르탄이에요',
+    'programming_languages': ['java', 'python', 'html'],
+    'strength': '멍떄리기',
+    'mbti': 'ISFP',
+    'tmi': '자전거 여행 좋아합니다~ 같이 하실분 DM 주세요~'
+}
 
 @app.route('/members/1')
 def members1():
-     return render_template('profle.html')
+    # member
 
+    # replace \n -> <br>
+    context['introduce'] = context['introduce'].replace('\n', '<br>')
+
+    return render_template('profle.html', data=context)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5002)
